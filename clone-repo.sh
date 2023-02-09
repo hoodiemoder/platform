c390859f1b989a4dfca46501eb52f3f83704e799
hoodiemoder <hoodiemoder@pm.me>
2023-02-09 20:02:37 +0000

    Add chone-repo.sh
    Script used for cloning my github repositories.

---
+++ clone-repo.sh
@@ -0,0 +1,12 @@
+mkdir github
+cd github
+git clone git@github.com:hoodiemoder/....git
+git clone git@github.com:hoodiemoder/art.git
+git clone git@github.com:hoodiemoder/branding.git
+git clone git@github.com:hoodiemoder/gleaming.git
+git clone git@github.com:hoodiemoder/hoodiemoder.git
+git clone git@github.com:hoodiemoder/hoodiemoder.com.git
+git clone git@github.com:hoodiemoder/platform.git
+git clone git@github.com:hoodiemoder/programming.git
+git clone git@github.com:hoodiemoder/resource.git
+git clone git@github.com:hoodiemoder/sdr.git
