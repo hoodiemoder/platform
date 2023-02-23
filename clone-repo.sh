91e1fe58b3ffb93a408c91c537bdd58ed2f2a624
hoodiemoder <hoodiemoder@pm.me>
2023-02-23 18:48:28 +0100

    Update clone-repo.sh

--- clone-repo.sh
+++ clone-repo.sh
@@ -1,12 +1,23 @@
-mkdir github
-cd github
+#!/bin/bash
+
+mkdir github  // Creates the github directory
+
+cd github  // Sets current directory to github
+
+
+
+// Cloning repositories, number of repos = n - 10
+
 git clone git@github.com:hoodiemoder/....git
 git clone git@github.com:hoodiemoder/art.git
 git clone git@github.com:hoodiemoder/branding.git
+git clone git@github.com:hoodiemoder/cs50.git
 git clone git@github.com:hoodiemoder/gleaming.git
 git clone git@github.com:hoodiemoder/hoodiemoder.git
 git clone git@github.com:hoodiemoder/hoodiemoder.com.git
 git clone git@github.com:hoodiemoder/platform.git
 git clone git@github.com:hoodiemoder/programming.git
 git clone git@github.com:hoodiemoder/resource.git
+git clone git@github.com:hoodiemoder/sicp.git
 git clone git@github.com:hoodiemoder/sdr.git
+git clone git@github.com:hoodiemoder/sicp.git
